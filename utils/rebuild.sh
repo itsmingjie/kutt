3c336ea2fd66095c6b50bbcc1d9592ddb14a4150
Mingjie Jiang <jiang@mingjie.info>
2019-12-21 20:37:20 -0500

    Update util

--- utils/rebuild.sh
+++ utils/rebuild.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-echo \n
-echo ==============================
-echo Pulling updates from repository...
+echo '=============================='
+echo 'Pulling updates from repository...'
 sudo git remote get-url origin 
 sudo git pull
 
-echo \n
-echo ==============================
-echo Rebuilding Server & Client...
+echo '=============================='
+echo 'Rebuilding Server & Client...'
 sudo npm run build
 
-echo \n
-echo ==============================
+echo '=============================='
 sudo service kutt restart
